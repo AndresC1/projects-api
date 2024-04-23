cfdcc7857b53ded60115cbdf69c92b1cc11ac958
AndresC1 <andrescornejolira@gmail.com>
2024-04-23 15:40:15 -0600

    fix: eliminacion de rutas no necesarias

--- services/auth/app.js
+++ services/auth/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { db } = require('./db.js');
 require('dotenv').config({ path: '.env.local' });
 const { authRoutes } = require('./routes/authRoutes');
 
@@ -10,27 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/api/v1/auth', authRoutes);
-
-app.get('/api/v1/db', async (req, res) => {
-    try{
-        await db.connect();
-        res.status(200).json("Conectado correctamente");
-    } catch (error){
-        res.status(500).send(error);
-    }
-});
-
-app.get('/api/v1/db/users', async (req, res) => {
-    try{
-        const request = {
-            query: req.query
-        }
-        const data = await db.findAll("users");
-        res.status(200).json(data);
-    } catch (error){
-        res.status(500).send(error);
-    }
-})
 
 app.listen(
     process.env.PORT || 3000,
